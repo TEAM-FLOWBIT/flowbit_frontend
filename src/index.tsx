bfe805e38b178d16fad4fc5d0ad3813e9f675fa5
ehdrbdndns <60888473+ehdrbdndns@users.noreply.github.com>
2023-12-02 18:38:30 +0900

    strict 모드 해제

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import router from "./router/Router";
-import GlobalStyles from "./styles/GlobalStyles";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import router from './router/Router';
+import GlobalStyles from './styles/GlobalStyles';
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <>
     <GlobalStyles />
     <RouterProvider router={router} />
-  </React.StrictMode>
+  </>
 );
